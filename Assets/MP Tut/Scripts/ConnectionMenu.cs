7bec9fa5a8ee47a3c6977764f1e035cadab06a7f
Asclepiius.Unknown <asclepiius.unknown@gmail.com>
2020-11-17 16:27:36 +1100

    multiplayer james template

---
+++ Assets/MP Tut/Scripts/ConnectionMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+using TMPro;
+
+public class ConnectionMenu : MonoBehaviour
+{
+    public TMP_InputField ipInput;
+
+    public void OnClickHost() => NetworkManager.singleton.StartHost();
+
+    public void OnClickConnect()
+    {
+        NetworkManager.singleton.networkAddress = string.IsNullOrEmpty(ipInput.text) ? "localhost" : ipInput.text;
+    }
+}
---
+++ Assets/Scripts/Drag & Drop/ItemSlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public GameObject currentObject;
+
+    public void OnDrop(PointerEventData _eventData)
+    {
+        if (_eventData.pointerDrag != null)
+        {
+            _eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            currentObject = _eventData.pointerDrag;
+            currentObject.GetComponent<DragDrop>().currentSlot = this;
+        }
+    }
+
+    public void ReleaseObject()
+    {
+        currentObject = null;
+    }
+}
